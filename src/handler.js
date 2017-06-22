bb9b7ba38176308cb3291acff65953a1ce0c2eef
Matt Scifo <matt@scifo.info>
2017-06-22 13:48:59 -0700

    Get rid of this, was mainly for debugging heap memory issues.

--- src/handler.js
+++ src/handler.js
@@ -26,7 +26,6 @@ module.exports.handle = (event, context, callback) => {
   var options = {
     stdio: ['pipe', 'pipe', 'pipe', 'pipe'], 
     env: {
-      USE_ZEND_ALLOC: '0', 
       AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID, 
       AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY, 
       AWS_SESSION_TOKEN: process.env.AWS_SESSION_TOKEN,
